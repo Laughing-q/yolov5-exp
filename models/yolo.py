d537a0a1a26631f3968af2dadd2ecbc69910f7a9
Laughing-q <1185102784@qq.com>
2022-12-20 16:45:53 +0000

    update channel

--- models/yolo.py
+++ models/yolo.py
@@ -52,7 +52,7 @@ class V6Detect(nn.Module):
         self.inplace = inplace  # use inplace ops (e.g. slice assignment)
         self.stride = torch.zeros(self.nl)  # strides computed during build
 
-        c2, c3 = max(ch[0] // 4, 16), max(ch[0], self.nc)  # channels
+        c2, c3 = max(ch[0] // 4, 16), max(ch[0], 128)  # channels
         self.cv2 = nn.ModuleList(
             nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
         self.cv3 = nn.ModuleList(
